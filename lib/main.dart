23d8899caf65ee4cac367cf6567c28532fb946a0
Rohan Taneja <rohan.bvcoe@gmail.com>
2018-03-24 14:44:00 +0530

    added separate classes for ChatApp and ChatScreen

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,33 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    new MaterialApp(
-      title: "Flutter Chat App",
-      home: new Scaffold(
-        appBar: new AppBar(
-          title: new Text("Flutter Chat App"),
-        ),
+  runApp(new MaterialApp(
+    title: "Flutter Chat App",
+    home: new Scaffold(
+      appBar: new AppBar(
+        title: new Text("Flutter Chat App"),
       ),
-    )
-  );
+    ),
+  ));
+}
+
+class FlutterChatApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: "Flutter Chat App",
+      home: new ChatScreen(),
+    );
+  }
+}
+
+class ChatScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text("Flutter Chat App"),
+      ),
+    );
+  }
 }
