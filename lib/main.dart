9985cf1da812a765d99e8f9549bc11e9adb8b88f
Rohan Taneja <rohan.bvcoe@gmail.com>
2018-03-24 23:54:15 +0530

    added margins to list view item

--- lib/main.dart
+++ lib/main.dart
@@ -98,17 +98,23 @@ class ChatMessage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new Container(
-      margin: const EdgeInsets.symmetric(horizontal: 8.0),
+      margin: const EdgeInsets.symmetric(vertical: 10.0),
       child: new Row(
-        crossAxisAlignment: CrossAxisAlignment.end,
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: <Widget>[
-          new CircleAvatar(
-            child: new Text(_name[0]), //display first letter of name in avatar
-          ),
+          new Container(
+              margin: const EdgeInsets.only(right: 16.0),
+              child: new CircleAvatar(
+                //display first letter of name in avatar
+                child: new Text(_name[0]),
+              )),
           new Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: <Widget>[
-              new Text(_name),
-              new Text(messageText),
+              new Text(_name, style: Theme.of(context).textTheme.subhead),
+              new Container(
+                  margin: const EdgeInsets.only(top: 5.0),
+                  child: new Text(messageText)),
             ],
           ),
         ],
