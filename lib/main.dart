1c372b54553c32ea6e25ee4b08d12ccef4ee7238
Rohan Taneja <rohan.bvcoe@gmail.com>
2018-03-24 23:35:47 +0530

    added cardColor background to sendMessage layout

--- lib/main.dart
+++ lib/main.dart
@@ -20,7 +20,6 @@ class ChatScreen extends StatefulWidget {
 }
 
 class ChatScreenState extends State<ChatScreen> {
-
   final List<ChatMessage> _messagesList = <ChatMessage>[];
   final TextEditingController _textEditingController =
       new TextEditingController();
@@ -28,11 +27,20 @@ class ChatScreenState extends State<ChatScreen> {
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-      appBar: new AppBar(
-        title: new Text("Flutter Chat App"),
-      ),
-      body: _buildTextComposer(),
-    );
+        appBar: new AppBar(
+          title: new Text("Flutter Chat App"),
+        ),
+        body: new Column(
+          children: <Widget>[
+            new Divider(
+                height: 1.0
+            ),
+            new Container(
+              decoration: new BoxDecoration(color: Theme.of(context).cardColor),
+              child: _buildTextComposer(),
+            )
+          ],
+        ));
   }
 
   Widget _buildTextComposer() {
@@ -65,12 +73,11 @@ class ChatScreenState extends State<ChatScreen> {
   }
 
   void _textMessageSubmitted(String text) {
-
     _textEditingController.clear();
     ChatMessage chatMessage = new ChatMessage(
       messageText: text,
     );
-    setState((){
+    setState(() {
       _messagesList.insert(0, chatMessage);
     });
   }
