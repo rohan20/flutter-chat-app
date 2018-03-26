96d19692369bead4ddb6bb518d063aeee52110d3
Rohan Taneja <rohan.bvcoe@gmail.com>
2018-03-26 23:38:37 +0530

    removed AppBar elevation for iOS and added decoration for send message layout

--- lib/main.dart
+++ lib/main.dart
@@ -61,39 +61,49 @@ class ChatScreenState extends State<ChatScreen> {
     return new Scaffold(
         appBar: new AppBar(
           title: new Text("Flutter Chat App"),
+          elevation: Theme.of(context).platform == TargetPlatform.iOS ? 0.0 : 4.0,
           actions: <Widget>[
             new IconButton(
                 icon: new Icon(Icons.exit_to_app), onPressed: _signOut)
           ],
         ),
-        body: new Column(
-          children: <Widget>[
-            new Flexible(
-              child: new FirebaseAnimatedList(
-                query: reference,
-                padding: const EdgeInsets.all(8.0),
-                reverse: true,
-                sort: (a, b) => b.key.compareTo(a.key),
-                //comparing timestamp of messages to check which one would appear first
-                itemBuilder: (_, DataSnapshot messageSnapshot,
-                    Animation<double> animation) {
-                  return new ChatMessage(
-                    messageSnapshot: messageSnapshot,
-                    animation: animation,
-                  );
-                },
+        body: new Container(
+          child: new Column(
+            children: <Widget>[
+              new Flexible(
+                child: new FirebaseAnimatedList(
+                  query: reference,
+                  padding: const EdgeInsets.all(8.0),
+                  reverse: true,
+                  sort: (a, b) => b.key.compareTo(a.key),
+                  //comparing timestamp of messages to check which one would appear first
+                  itemBuilder: (_, DataSnapshot messageSnapshot,
+                      Animation<double> animation) {
+                    return new ChatMessage(
+                      messageSnapshot: messageSnapshot,
+                      animation: animation,
+                    );
+                  },
+                ),
               ),
-            ),
-            new Divider(height: 1.0),
-            new Container(
-              decoration: new BoxDecoration(color: Theme.of(context).cardColor),
-              child: _buildTextComposer(),
-            ),
-            new Builder(builder: (BuildContext context) {
-              _scaffoldContext = context;
-              return new Container(width: 0.0, height: 0.0);
-            })
-          ],
+              new Divider(height: 1.0),
+              new Container(
+                decoration: new BoxDecoration(color: Theme.of(context).cardColor),
+                child: _buildTextComposer(),
+              ),
+              new Builder(builder: (BuildContext context) {
+                _scaffoldContext = context;
+                return new Container(width: 0.0, height: 0.0);
+              })
+            ],
+          ),
+          decoration: Theme.of(context).platform == TargetPlatform.iOS ? new BoxDecoration(
+            border: new Border(
+              top: new BorderSide(
+                color: Colors.grey[200],
+              )
+            )
+          ) : null,
         ));
   }
 
