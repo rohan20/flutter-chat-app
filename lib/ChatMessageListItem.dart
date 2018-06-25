da9d83bdf085f2d9ba59edde80495aa0b958fd46
Rohan Taneja <rohan.bvcoe@gmail.com>
2018-06-25 14:16:35 +0530

    renamed ChatMessage list item and added it to a separate file

---
+++ lib/ChatMessageListItem.dart
@@ -0,0 +1,101 @@
+import 'package:firebase_database/firebase_database.dart';
+import 'package:flutter/material.dart';
+
+var currentUserEmail;
+
+class ChatMessageListItem extends StatelessWidget {
+  final DataSnapshot messageSnapshot;
+  final Animation animation;
+
+  ChatMessageListItem({this.messageSnapshot, this.animation});
+
+  @override
+  Widget build(BuildContext context) {
+    return new SizeTransition(
+      sizeFactor:
+      new CurvedAnimation(parent: animation, curve: Curves.decelerate),
+      child: new Container(
+        margin: const EdgeInsets.symmetric(vertical: 10.0),
+        child: new Row(
+          children: currentUserEmail == messageSnapshot.value['email']
+              ? getSentMessageLayout()
+              : getReceivedMessageLayout(),
+        ),
+      ),
+    );
+  }
+
+  List<Widget> getSentMessageLayout() {
+    return <Widget>[
+      new Expanded(
+        child: new Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: <Widget>[
+            new Text(messageSnapshot.value['senderName'],
+                style: new TextStyle(
+                    fontSize: 14.0,
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold)),
+            new Container(
+              margin: const EdgeInsets.only(top: 5.0),
+              child: messageSnapshot.value['imageUrl'] != null
+                  ? new Image.network(
+                messageSnapshot.value['imageUrl'],
+                width: 250.0,
+              )
+                  : new Text(messageSnapshot.value['text']),
+            ),
+          ],
+        ),
+      ),
+      new Column(
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: <Widget>[
+          new Container(
+              margin: const EdgeInsets.only(left: 8.0),
+              child: new CircleAvatar(
+                backgroundImage:
+                new NetworkImage(messageSnapshot.value['senderPhotoUrl']),
+              )),
+        ],
+      ),
+    ];
+  }
+
+  List<Widget> getReceivedMessageLayout() {
+    return <Widget>[
+      new Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          new Container(
+              margin: const EdgeInsets.only(right: 8.0),
+              child: new CircleAvatar(
+                backgroundImage:
+                new NetworkImage(messageSnapshot.value['senderPhotoUrl']),
+              )),
+        ],
+      ),
+      new Expanded(
+        child: new Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            new Text(messageSnapshot.value['senderName'],
+                style: new TextStyle(
+                    fontSize: 14.0,
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold)),
+            new Container(
+              margin: const EdgeInsets.only(top: 5.0),
+              child: messageSnapshot.value['imageUrl'] != null
+                  ? new Image.network(
+                messageSnapshot.value['imageUrl'],
+                width: 250.0,
+              )
+                  : new Text(messageSnapshot.value['text']),
+            ),
+          ],
+        ),
+      ),
+    ];
+  }
+}
--- lib/ChatScreen.dart
+++ lib/ChatScreen.dart
@@ -1,6 +1,5 @@
 import 'dart:async';
 import 'dart:io';
-import 'dart:math';
 
 import 'package:firebase_analytics/firebase_analytics.dart';
 import 'package:firebase_auth/firebase_auth.dart';
@@ -9,6 +8,7 @@ import 'package:firebase_database/ui/firebase_animated_list.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_chat_app/ChatMessageListItem.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:image_picker/image_picker.dart';
 
@@ -55,7 +55,7 @@ class ChatScreenState extends State<ChatScreen> {
                   //comparing timestamp of messages to check which one would appear first
                   itemBuilder: (_, DataSnapshot messageSnapshot,
                       Animation<double> animation) {
-                    return new ChatMessage(
+                    return new ChatMessageListItem(
                       messageSnapshot: messageSnapshot,
                       animation: animation,
                     );
@@ -207,102 +207,5 @@ class ChatScreenState extends State<ChatScreen> {
     Scaffold
         .of(_scaffoldContext)
         .showSnackBar(new SnackBar(content: new Text('User logged out')));
-  }
-}
-
-class ChatMessage extends StatelessWidget {
-  final DataSnapshot messageSnapshot;
-  final Animation animation;
-
-  ChatMessage({this.messageSnapshot, this.animation});
-
-  @override
-  Widget build(BuildContext context) {
-    return new SizeTransition(
-      sizeFactor:
-          new CurvedAnimation(parent: animation, curve: Curves.decelerate),
-      child: new Container(
-        margin: const EdgeInsets.symmetric(vertical: 10.0),
-        child: new Row(
-          children: currentUserEmail == messageSnapshot.value['email']
-              ? getSentMessageLayout()
-              : getReceivedMessageLayout(),
-        ),
-      ),
-    );
-  }
-
-  List<Widget> getSentMessageLayout() {
-    return <Widget>[
-      new Expanded(
-        child: new Column(
-          crossAxisAlignment: CrossAxisAlignment.end,
-          children: <Widget>[
-            new Text(messageSnapshot.value['senderName'],
-                style: new TextStyle(
-                    fontSize: 14.0,
-                    color: Colors.black,
-                    fontWeight: FontWeight.bold)),
-            new Container(
-              margin: const EdgeInsets.only(top: 5.0),
-              child: messageSnapshot.value['imageUrl'] != null
-                  ? new Image.network(
-                      messageSnapshot.value['imageUrl'],
-                      width: 250.0,
-                    )
-                  : new Text(messageSnapshot.value['text']),
-            ),
-          ],
-        ),
-      ),
-      new Column(
-        crossAxisAlignment: CrossAxisAlignment.end,
-        children: <Widget>[
-          new Container(
-              margin: const EdgeInsets.only(left: 8.0),
-              child: new CircleAvatar(
-                backgroundImage:
-                    new NetworkImage(messageSnapshot.value['senderPhotoUrl']),
-              )),
-        ],
-      ),
-    ];
-  }
-
-  List<Widget> getReceivedMessageLayout() {
-    return <Widget>[
-      new Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          new Container(
-              margin: const EdgeInsets.only(right: 8.0),
-              child: new CircleAvatar(
-                backgroundImage:
-                    new NetworkImage(messageSnapshot.value['senderPhotoUrl']),
-              )),
-        ],
-      ),
-      new Expanded(
-        child: new Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            new Text(messageSnapshot.value['senderName'],
-                style: new TextStyle(
-                    fontSize: 14.0,
-                    color: Colors.black,
-                    fontWeight: FontWeight.bold)),
-            new Container(
-              margin: const EdgeInsets.only(top: 5.0),
-              child: messageSnapshot.value['imageUrl'] != null
-                  ? new Image.network(
-                      messageSnapshot.value['imageUrl'],
-                      width: 250.0,
-                    )
-                  : new Text(messageSnapshot.value['text']),
-            ),
-          ],
-        ),
-      ),
-    ];
   }
 }
